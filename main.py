e55318a519e4561d425f840ff7bac127d69c1289
atea801 <58953366+atea801@users.noreply.github.com>
2024-01-29 09:08:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from data_preparation import DataPreparation
 from regression import Regression
 
 
-csv_path = "./number of travelers.csv"
-data_preparation_object = DataPreparation(csv_path)
-regression_object = Additif(data_preparation_object)
-
-# data_preparation_object.show_graph()
+csv_path = 'vente_maillots_de_bain.csv'
+data_prep = DataPreparation(csv_path)
+regression_model = Regression(data_prep)
